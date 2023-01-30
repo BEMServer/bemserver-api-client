c114b00c22455aafead4f00d5958575ad359748f
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-01-30 18:46:32 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     python_requires=">=3.9",
     install_requires=[
         "requests>=2.27.1",
-        "packaging>=21.3",
+        "packaging>=23.0",
     ],
     packages=find_packages(exclude=["tests*"]),
 )
