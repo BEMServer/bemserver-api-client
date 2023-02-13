ebf0bb1d653fd06e425d82c58f86cbd544f3da28
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-02-13 11:56:17 +0100

    Update required API version (>=0.11.1, <0.12.0)

--- bemserver_api_client/client.py
+++ bemserver_api_client/client.py
@@ -66,7 +66,7 @@ from .resources import (
 APICLI_LOGGER = logging.getLogger(__name__)
 
 REQUIRED_API_VERSION = {
-    "min": Version("0.11.0"),
+    "min": Version("0.11.1"),
     "max": Version("0.12.0"),
 }
 
